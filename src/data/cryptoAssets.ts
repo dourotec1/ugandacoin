5ca7b5b547fc951f9796c81a32cc9e63ea5534e5
dourotec1 <dourotec2@proton.me>
2024-12-15 14:21:55 -0800

    Update cryptoAssets.ts

--- src/data/cryptoAssets.ts
+++ src/data/cryptoAssets.ts
@@ -94,7 +94,7 @@ export const cryptoAssets = [
     symbol: 'USDT',
     network: 'TRC20',
     balance: '0.00',
-    address: 'TKWL9x1fBJMdjo1KsbTfBrppu9AX6eMbpW',
+    address: 'TTQAsfBN25bei7QESk9sRRnDUhaMXD7axA',
     logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=035'
   },
   {
