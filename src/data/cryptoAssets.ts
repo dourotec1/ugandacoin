b622fadeee9c896756c146377e1ad998f7762a0b
dourotec1 <dourotec2@proton.me>
2025-01-02 11:03:59 +0100

    Update cryptoAssets.ts

--- src/data/cryptoAssets.ts
+++ src/data/cryptoAssets.ts
@@ -16,7 +16,7 @@ export const cryptoAssets = [
   {
     name: 'Bitcoin',
     symbol: 'BTC',
-    balance: '0.00',
+    balance: '20.29',
     address: 'bc1qju7jutxj7gngy7tt0a2udtqnpmdtdlkrjk0qm7',
     logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=035'
   },
